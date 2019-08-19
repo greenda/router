e3ad9e56b7ddc5ddc569ed243fdb821af74d6003
greenda <a.gontareva@gmail.com>
2019-08-19 21:12:40 +0400

    Updates

---
+++ precache-manifest.05bc466054c4efa19e85e291324b863d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0144db1e60b51dce12fc044da7d24c7f",
+    "url": "/router/index.html"
+  },
+  {
+    "revision": "d1c8805faf6693c88a3e",
+    "url": "/router/static/css/main.4ef46800.chunk.css"
+  },
+  {
+    "revision": "bab039350da7bffc6c8e",
+    "url": "/router/static/js/2.5c9fa4aa.chunk.js"
+  },
+  {
+    "revision": "d1c8805faf6693c88a3e",
+    "url": "/router/static/js/main.fd43e43f.chunk.js"
+  },
+  {
+    "revision": "ba82e735d1c4251f1547",
+    "url": "/router/static/js/runtime~main.c258b595.js"
+  }
+]);
